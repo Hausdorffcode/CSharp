6ea0ebd3dd2f89d86ea0d49fbdd3f5c2a8175154
Hausdorffcode <m15950172843@163.com>
2016-12-05 11:53:24 +0800

    索引器使用
    练习使用索引器

---
+++ propertyAndIndexer/propertyAndIndexer/IndexerRecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertyAndIndexer
+{
+    class IndexerRecord
+    {
+        private string[] date = new string[6];
+        private string[] keys = { "Author", "Publisher", "Title", "Subject", "ISBN", "Comment" };
+
+        public string this[int idx]
+        {
+            set
+            {
+                if (idx >= 0 && idx < date.Length)
+                    date[idx] = value;
+            }
+            get
+            {
+                if (idx >= 0 && idx < date.Length)
+                    return date[idx];
+                return null;
+            }
+        }
+
+        public string this[string key]
+        {
+            set
+            {
+                int idx = findIndex(key);
+                this[idx] = value;     //访问的是date
+            }
+            get
+            {
+                return this[findIndex(key)];   //访问的是date
+            }
+        }
+
+        private int findIndex(string key)
+        {
+            for(int i = 0; i < keys.Length; i++)
+            {
+                if (key == keys[i])
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ propertyAndIndexer/propertyAndIndexer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertyAndIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexerRecord indexer = new IndexerRecord();
+            indexer[0] = "马克吐温";      //索引器set
+            indexer[1] = "Crox出版公司";
+            indexer[2] = "汤姆索亚历险";
+
+            Console.WriteLine(indexer["Title"]); //索引访问， get
+            Console.WriteLine(indexer["Author"]);
+            Console.WriteLine(indexer["Publisher"]);
+
+            Console.ReadKey();
+        }
+    }
+}
